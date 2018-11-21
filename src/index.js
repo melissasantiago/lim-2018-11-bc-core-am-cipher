bab9b877553e0888c43ac1a63a442fb28e6bcb3d
melissasantiago <meli235ssa@gmail.com>
2018-11-21 13:04:19 -0500

    descifrado

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
+
+
+
+
+const desplazar1 =document.getElementById('desplazar1');
+const 
+
+
+
+
+
 function cifrar(cifrarLe,desplaz){
   let resultado = '';
 
   for (let i = 0 ; i<cifrarLe.length ; i++){
-   const asc = cifrarLe.charCodeAt(i);
+   let asc = cifrarLe.charCodeAt(i);
    const formula = (asc - 65 + desplaz)%26 + 65;
-
-  resultado = resultado + String.fromCharCode(formula);
+   resultado = resultado + String.fromCharCode(formula);
 }
-
  return resultado;
 }
+console.log(cifrar('AMOR',100));
 
-console.log(cifrar('AMOR',7));
+//funcion para descifrar
+function dscrifrar(dscifrarLe,desplaz){
+ let dsresultado = '';
+ let newdesplaz = (desplaz%26);
+for (let i = 0; i<dscifrarLe.length; i++){
+      let desc = dscifrarLe.charCodeAt(i);
+      const formula = (desc+ 65-newdesplaz)%26+65;
+      dsresultado = dsresultado + String.fromCharCode(formula);
+}
+    return dsresultado;
+}
+console.log(dscrifrar('WIKN',100));
