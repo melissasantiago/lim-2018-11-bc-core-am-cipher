35a0767d31e325933ba8e56b37caf7966526e5bc
melissa <meli235ssa@gmail.com>
2018-11-22 21:22:33 -0500

    boton limpiar

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,27 @@
 window.cipher = {
-  // ... 
+
+encode: (cifrarLe,desplaz)=>
+  {
+  let resultado = '';
+
+  for (let i = 0 ; i<cifrarLe.length ; i++){
+  let asc = cifrarLe.charCodeAt(i);
+   let formula = (asc - 65 + desplaz )% 26 + 65;
+   resultado =resultado + String.fromCharCode(formula);
+}
+ return resultado;
+},
+
+
+decode: (dscifrarLe,desplaz) =>
+  {
+    let dsresultado = '';
+    let newdesplaz = (desplaz%26);
+    for (let i = 0; i<dscifrarLe.length; i++){
+        let desc = dscifrarLe.charCodeAt(i);
+        let formula = (desc + 65 - newdesplaz)%26+65;
+        dsresultado = dsresultado + String.fromCharCode(formula);
+    }
+    return dsresultado;
+}
 };
--- src/index.js
+++ src/index.js
@@ -1,25 +1,37 @@
 
-
-
-
 const desplazar1 =document.getElementById('desplazar1');
-const 
+const text1 = document.getElementById('text1');
+const btnCifra = document.getElementById('btnCifra');
+const btnDscifra = document.getElementById('btnDscifra');
+const text2 = document.getElementById('text2');
+
+
+btnCifra.addEventListener('click',llamarFuncion);
+btnDscifra.addEventListener('click',llamarDsfuncion);
 
 
 
+function llamarFuncion(){
+text2.value =cifrar(text1.value,parseInt(desplazar1.value));
+}
+
+
+function llamarDsfuncion(){
+text2.value =dscrifrar(text1.value,parseInt(desplazar1.value));
+}
 
 
 function cifrar(cifrarLe,desplaz){
   let resultado = '';
 
   for (let i = 0 ; i<cifrarLe.length ; i++){
-   let asc = cifrarLe.charCodeAt(i);
-   const formula = (asc - 65 + desplaz)%26 + 65;
-   resultado = resultado + String.fromCharCode(formula);
+  let asc = cifrarLe.charCodeAt(i);
+   let formula = (asc - 65 + desplaz )% 26 + 65;
+   resultado =resultado + String.fromCharCode(formula);
 }
  return resultado;
 }
-console.log(cifrar('AMOR',100));
+
 
 //funcion para descifrar
 function dscrifrar(dscifrarLe,desplaz){
@@ -27,9 +39,8 @@ function dscrifrar(dscifrarLe,desplaz){
  let newdesplaz = (desplaz%26);
 for (let i = 0; i<dscifrarLe.length; i++){
       let desc = dscifrarLe.charCodeAt(i);
-      const formula = (desc+ 65-newdesplaz)%26+65;
+      let formula = (desc + 65 - newdesplaz)%26+65;
       dsresultado = dsresultado + String.fromCharCode(formula);
 }
     return dsresultado;
-}
-console.log(dscrifrar('WIKN',100));
+ }
